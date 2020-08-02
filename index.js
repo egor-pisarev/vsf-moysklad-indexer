081f6b7c194196b7efa84bf0d8c55adce49fde59
egor.pisarev <egor.pisarev@gmail.com>
2020-08-02 12:24:39 +0400

    changes

--- index.js
+++ index.js
@@ -10,37 +10,49 @@ const { loader, client } = require('./src/sources/ms/loaders/loader')(config)
 
 const clearCustomers = async () => {
 
-    console.log('Start indexer')
+    let startTime = new Date().getTime()
+
+    console.log('Start indexer', new Date())
 
     let customers = []
     let emails = []
+    let counter = 0
 
     await loader(`https://online.moysklad.ru/api/remap/1.2/entity/customerorder?offset=0&limit=100`, 'customerorders', (row) => {
-        console.log('Push customer ',row.agent.meta.href)
         customers.push(row.agent.meta.href)
     })
 
+    let list = []
     let part = []
-    let counter = 1
+    let startPartTime = new Date().getTime()
 
-    await loader(`https://online.moysklad.ru/api/remap/1.2/entity/counterparty?offset=0&limit=500`, 'customers', async (row) => {
-        if(counter % 1000 === 0){
-            console.log('Deleted', counter)
-        }
+    await loader(`https://online.moysklad.ru/api/remap/1.2/entity/counterparty?offset=0&limit=1000&order=updated,asc`, 'customers', async (row) => {
         if(row.email && emails.indexOf(row.email) >= 0){
             if (customers.indexOf(row.meta.href) < 0) {
-                counter++
-                part.push(client.delete(row.meta.href).catch(err => console.log(err.message)))
+                list.push({meta: row.meta})
             }
         } else {
-            //logger.warn(JSON.stringify(row))
             emails.push(row.email)
         }
+
+        if(list.length === 200){
+            part.push(client.post(`https://online.moysklad.ru/api/remap/1.2/entity/counterparty/delete`, list)
+            .then(r=>console.log('Deleted 200.', `${(new Date().getTime() - startPartTime)/1000} sec`))
+            .catch(err => console.log('Delete  error',err.message)))
+            list = []
+        }
+
         if(part.length === 10){
-            await Promise.all(part)
+            startPartTime = new Date().getTime()
+            console.log('Start DELETE part ')
+            await Promise.all(part).then(r=>console.log('Deleted Part Total Script time', `${(new Date().getTime() - startTime)/1000} sec`)).catch(err=>console.log('Delete Part error',err.message))
             part = []
+            counter+=2000
+            console.log('Total deleted ',counter)
         }
     })
+
+
 
 }
 
