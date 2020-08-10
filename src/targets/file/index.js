c763d002ba74a65490ea5a834e418f960a52cb68
egor.pisarev <egor.pisarev@gmail.com>
2020-08-10 23:53:00 +0400

    i18n added

--- src/targets/file/index.js
+++ src/targets/file/index.js
@@ -20,9 +20,20 @@ module.exports = (config, utils) => {
 
     const run = async (entities) => {
 
-        configData.products.defaultFilters = Object.keys(entities.attribute).map(attr=>`attribute_${attr}`)
+        let i18n = ``
+
+        Object.keys(entities.attribute).map(name => {
+            i18n = `${i18n}
+"attribute_${name}_filter","${entities.attribute[name].label}"`
+        })
+
+        fs.writeFile(`${RESULTS_PATH}/i18n.json`, i18n, () => console.log(`i18n wrote`))
+
+        configData.products.defaultFilters = []
         configData.products.defaultFilters.push("price")
         configData.products.defaultFilters.push("stock.is_in_stock")
+        
+        Object.keys(entities.attribute).map(attr=>configData.products.defaultFilters.push(`attribute_${attr}`))
 
         fs.writeFile(CONFIG_PATH, JSON.stringify(configData), () => console.log(`Config file wrote to ${CONFIG_PATH}`))
 
